63592f3c65ac771eef8a1a18e5b0c95284cd8f59
Nicholas Chumney <nicholas.chumney@outlook.com>
2020-11-03 19:58:56 -0500

    replaced connect with hooks in NewsApp

--- src/containers/NewsApp/NewsApp.js
+++ src/containers/NewsApp/NewsApp.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Articles from '../../components/Articles';
 import SearchBar from '../../components/SearchBar';
 import {
@@ -9,42 +9,32 @@ import {
   fetchPrevPage,
 } from '../../store/actions';
 
-function NewsApp(props) {
-  const { fetchTopHeadlines } = props;
+const NewsApp = () => {
+  const news = useSelector((state) => {
+    return state.news;
+  });
+
+  const dispatch = useDispatch();
+  const onInit = useCallback(() => dispatch(fetchTopHeadlines()), [dispatch]);
+  const onSearch = (search) => dispatch(fetchHeadlinesBySearch(search));
+  const onNextPage = () => dispatch(fetchNextPage());
+  const onPrevPage = () => dispatch(fetchPrevPage());
 
   useEffect(() => {
-    fetchTopHeadlines();
-  }, [fetchTopHeadlines]);
+    onInit();
+  }, [onInit]);
 
   return (
     <div>
-      <h1>News App</h1>
-      <SearchBar makeSearch={props.fetchHeadlinesBySearch} />
-      <Articles articles={props.news.articles} />
-      {props.news.page > 1 && (
-        <button onClick={props.fetchPrevPage}>Prev</button>
-      )}
-      {props.news.page < props.news.totalPages && (
-        <button onClick={props.fetchNextPage}>Next</button>
+      <h1>Newsly</h1>
+      <SearchBar makeSearch={onSearch} />
+      <Articles articles={news.articles} />
+      {news.page > 1 && <button onClick={onPrevPage}>Prev</button>}
+      {news.page < news.totalPages && (
+        <button onClick={onNextPage}>Next</button>
       )}
     </div>
   );
-}
-
-const mapStateToProps = (state) => {
-  return {
-    news: state.news,
-  };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchTopHeadlines: () => dispatch(fetchTopHeadlines()),
-    fetchHeadlinesBySearch: (search) =>
-      dispatch(fetchHeadlinesBySearch(search)),
-    fetchNextPage: () => dispatch(fetchNextPage()),
-    fetchPrevPage: () => dispatch(fetchPrevPage()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(NewsApp);
+export default NewsApp;
